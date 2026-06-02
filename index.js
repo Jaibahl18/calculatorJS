d2b1a7f75cbb24e59cf59d20a46749888a7e3e98
Jaibahl18 <bahljai2006@gmail.com>
2026-06-03 02:39:08 +0530

    v1

---
+++ index.js
@@ -0,0 +1,22 @@
+const textBar = document.querySelector("#textBar");
+
+function textAppend(value) {
+    textBar.value = textBar.value + (value);
+}
+
+function equal() {
+    textBar.value = compute();
+}
+
+function clearText() {
+    textBar.value = '';
+}
+
+function compute() {
+    try {
+        return eval(textBar.value);
+    }
+    catch {
+        return "Error"
+    }
+}
